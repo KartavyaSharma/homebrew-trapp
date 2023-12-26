630b83eb3bb48e0679e5547ea57c22063ca3aaf7
Kartavya Sharma <kartavya@berkeley.edu>
2023-12-26 01:04:44 -0800

    Added trapp.rb file

--- Formula/trapp.rb
+++ Formula/trapp.rb
@@ -1,17 +1,50 @@
 class Trapp < Fomula
   desc "Automated Job Application Tracker"
   homepage "trapp.sh"
-  url ""
-  sha256 ""
+  url "https://github.com/KartavyaSharma/trapp/releases/download/Production/trapp-v1.0.0.tar.gz"
+  sha256 "2e31db0a223f0cffdbde25041a345096bc9ab8bf13a58c059d3bb9395efdd653"
 
-  depends_on ""
-  
-  def install
-    # Evironment settings, compilation, etc.
-    
+  depends_on "bash"
+  depends_on "bat"
+  depends_on "docker"
+  depends_on "gum"
+  depends_on "python3"
+  depends_on "rm-improved"
+  depends_on "tar"
+  depends_on "unzip"
+  depends_on "wget"
+
+  if OS.mac?
+    depends_on "colima"
+    depends_on "coreutils"
+    depends_on "google-chrome"
+    depends_on "openssl@1.1"
+  elsif OS.linux?
+    depends_on "build-essential"
+    depends_on "python3-pip"
+    depends_on "python3-setuptools"
+    depends_on "python3-venv"
   end
   
+  def install
+    # Create the ~/.trapp directory
+    trapp_home = Pathname.new("#{ENV["HOME"]}/.trapp")
+    mkdir_p trapp_home
+  
+    # Extract all files from the tarball into the ~/.trapp directory
+    system "tar", "-xzf", "trapp-v1.0.0.tar.gz", "-C", trapp_home
+  
+    # Create the trapp command script
+    (bin/"trapp").write <<~EOS
+      #!/bin/bash
+      #{trapp_home}/start.sh
+    EOS
+  
+    # Ensure the start.sh script is executable
+    chmod 0755, trapp_home/"start.sh"
+  end 
+  
   test do
-    # Run tests to validate installation  
+    system "trapp", "--version"
   end
 end
