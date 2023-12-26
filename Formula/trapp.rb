97ad8429ec6caa3aca110714e73295ddd39c7269
Kartavya Sharma <kartavya@berkeley.edu>
2023-12-25 17:05:34 -0800

    Added basic formula file for trapp

---
+++ Formula/trapp.rb
@@ -0,0 +1,17 @@
+class Trapp < Fomula
+  desc "Automated Job Application Tracker"
+  homepage "trapp.sh"
+  url ""
+  sha256 ""
+
+  depends_on ""
+  
+  def install
+    # Evironment settings, compilation, etc.
+    
+  end
+  
+  test do
+    # Run tests to validate installation  
+  end
+end
