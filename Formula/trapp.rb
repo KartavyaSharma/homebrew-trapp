7f262b87a438a7c5bbde3ec4db7e9ce37f141598
Kartavya Sharma <kartavya@berkeley.edu>
2023-12-30 01:59:44 -0800

    Update trapp formula to version f8be5921d47360336eceb522ab40bbacf09eceaca13722b874ef577e43c8a7dd

--- Formula/trapp.rb
+++ Formula/trapp.rb
@@ -1,44 +1,44 @@
 class Trapp < Formula
-  desc "Automated Job Application Tracker"
-  homepage "trapp.sh"
-  url "https://github.com/KartavyaSharma/trapp/releases/download/Production/trapp-v1.0.0.tar.gz"
-  sha256 "f7f98d6cbd02495fdb844d62854521da8c837e611216483db7666e6fe7544125"
 
-  depends_on "bash"
-  depends_on "bat"
-  depends_on "docker"
-  depends_on "gum"
-  depends_on "python3"
-  depends_on "rm-improved"
-  depends_on "gnu-tar"
-  depends_on "unzip"
-  depends_on "wget"
+    desc "Automated Job Application Tracker on the CLI"
+    homepage "trapp.sh"
+    url "https://github.com/KartavyaSharma/trapp/releases/download/Production/trapp-v1.0.0.tar.gz"
+    sha256 "f8be5921d47360336eceb522ab40bbacf09eceaca13722b874ef577e43c8a7dd"
 
-  if OS.mac?
-    depends_on "colima"
-    depends_on "coreutils"
-    depends_on "openssl@1.1"
-  elsif OS.linux?
-    depends_on "build-essential"
-    depends_on "python3-pip"
-    depends_on "python3-setuptools"
-    depends_on "python3-venv"
-  end
- 
-  def install
-    # Place all project files in the package's libexec directory
-    libexec.install Dir["*"]
-    
-    # Write an executable script that runs your `start.sh` script
-    (bin/"trapp").write <<~EOS
-      #!/bin/bash
-      exec "#{libexec}/start.sh" "$@"
-    EOS
-    bin.install_symlink "#{libexec}/start.sh" => "trapp"
-  end
+    depends_on "bash"
+    depends_on "bat"
+    depends_on "docker"
+    depends_on "gum"
+    depends_on "python3"
+    depends_on "rm-improved"
+    depends_on "gnu-tar"
+    depends_on "gawk"
+    depends_on "unzip"
+    depends_on "wget"
 
-  test do
-    # Replace with a suitable test, e.g., checking output of a command
-    system "trapp", "--help"
-  end
+    if OS.mac?
+        depends_on "coreutils"
+        depends_on "colima"
+        depends_on "openssl@1.1"
+    elsif OS.linux?
+        depends_on "build-essential"
+        depends_on "python3-pip"
+        depends_on "python3-setuptools"
+        depends_on "python3-venv"
+    end
+
+    def install
+        libexec.install Dir["*"]
+
+        (bin/"{name}").write <<~EOS
+            #!/bin/bash
+            exec "#{libexec}/start.sh" "$@"
+        EOS
+        bin.install_symlink "#{libexec}/start.sh" => "#{name}"
+    end
+
+    test do
+        system "{name}", "--help"
+    end
+
 end
