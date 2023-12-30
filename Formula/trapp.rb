a3034c7e553c852543ce74bd1b762725cf685c89
Kartavya Sharma <kartavya@berkeley.edu>
2023-12-30 03:55:01 -0800

    Update trapp formula to version d0644edf81966b7016f9821c476530e9eac9c4c11078aa562c018c29773d0eae

--- Formula/trapp.rb
+++ Formula/trapp.rb
@@ -3,7 +3,7 @@ class Trapp < Formula
     desc "Automated Job Application Tracker on the CLI"
     homepage "trapp.sh"
     url "https://github.com/KartavyaSharma/trapp/releases/download/Production/trapp-v1.0.0.tar.gz"
-    sha256 "237799edb962255a3b8ecbb15e424d3b5a6f6e69eb5cd18d21d85d70c2b8b786"
+    sha256 "d0644edf81966b7016f9821c476530e9eac9c4c11078aa562c018c29773d0eae"
 
     depends_on "bash"
     depends_on "bat"
